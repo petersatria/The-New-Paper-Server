325c11d17ce76f2065fd589d32825d7a915ce680
petersatria <180710018@students.uajy.ac.id>
2023-05-15 20:44:28 +0700

    feat: add bcrypt in helpers & implement bycrypt in user seeders

--- seeders/20230515093516-users.js
+++ seeders/20230515093516-users.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const { hashPassword } = require("../helpers/bcrypt");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const data = require('../data/users.json')
     data.forEach(e => {
       e.createdAt = e.updatedAt = new Date()
+      e.password = hashPassword(e.password)
     })
     await queryInterface.bulkInsert('Users', data);
   },
