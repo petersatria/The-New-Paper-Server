866728c6459684099a456eef4944bfd224966706
petersatria <180710018@students.uajy.ac.id>
2023-05-17 19:02:18 +0700

    chore: add cors in app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(require('./routes'))
