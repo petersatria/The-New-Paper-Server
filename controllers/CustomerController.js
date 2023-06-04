e7b5465d3c618502e74f254b86f394b1c7fd9d28
petersatria <180710018@students.uajy.ac.id>
2023-06-04 22:54:01 +0700

    fix: url qr code to deployed client

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -170,7 +170,7 @@ class CustomerController {
         url: `https://api.qr-code-generator.com/v1/create?access-token=${process.env.QR_TOKEN}`,
         data: {
           "frame_name": "no-frame",
-          "qr_code_text": "http://localhost:8080/articles/" + id,
+          "qr_code_text": "https://thenewpaper-news.web.app/articles/" + id,
           "image_format": "SVG",
           "qr_code_logo": "scan-me-square"
         }
